bc9a96db0bd9e4d7d3c12b70ed900b8a11ff15c1
Rampant91 <rampant91@gmail.com>
2022-07-25 11:58:24 +0300

    AddModels

---
+++ ContactBook_ViewsNavigation/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook_ViewsNavigation.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Patronymic { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public IEnumerable<Order>? Orders { get; set; }
+    }
+}
---
+++ ContactBook_ViewsNavigation/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook_ViewsNavigation.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public int ContactId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Amount { get; set; }
+
+        public string? Discription { get; set; }
+    }
+}
